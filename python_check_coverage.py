a7aacb718246ff70eb47c6369b193606cc359d34
Billy Peake <b.peake@illuscio.com>
2020-05-20 14:12:18 -0700

    python coverage requirement now comes from config

--- python_check_coverage.py
+++ python_check_coverage.py
@@ -1,21 +1,28 @@
 import sys
 import pathlib
 import argparse
+import configparser
 from xml.etree import ElementTree as et
 
 MODULE_DIR = pathlib.Path("./").absolute()
 
+
+def load_cfg(config_path: pathlib.Path) -> configparser.ConfigParser:
+    """
+    loads library config file
+    :return: loaded `ConfigParser` object
+    """
+    config = configparser.ConfigParser()
+    config.read(str(config_path))
+    return config
+
+
 if __name__ == "__main__":
+    # Get the main package name.
+    config = load_cfg(pathlib.Path("./setup.cfg").absolute())
+    package_name = config.get("metadata", "name")
 
-    arg_parser = argparse.ArgumentParser(description="check coverage")
-    arg_parser.add_argument(
-        "--minimum",
-        type=float,
-        help="minimum coverage % required to build"
-    )
-    args = arg_parser.parse_args()
-
-    minimum_coverage = args.minimum
+    minimum_coverage = config.getfloat("testing", "coverage_required", fallback=0.85)
     print(f"COVERAGE REQUIRED: {minimum_coverage * 100}%")
 
     coverage_path = (
@@ -23,7 +30,7 @@ if __name__ == "__main__":
     )
 
     cov_xml: et.Element = et.parse(coverage_path).getroot()
-    package_info: et.Element = cov_xml.find(".//package[@name='.']")
+    package_info: et.Element = cov_xml.find(f".//package[@name='{package_name}']")
 
     coverage = float(package_info.attrib["line-rate"])
 
